33a47de51621b4b83793bee1c0f8e26e74a27d52
Enrique Marina <eherraiz@myuax.com>
2022-07-01 23:28:15 +0200

    continuamos

--- warrior.py
+++ warrior.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from Vida import Vida
 class Tipos_de_guerrero(Enum):
     Boxeador = 0
     Gladiador = 1
@@ -12,13 +13,13 @@ class Tipos_de_ataque(Enum):
     Espada = 10
 
 
-class Warriors():
+class Warrior(Vida):
     def __init__(self, tipo, arma, salud, ataque, defensa):
         # Generamos excepciones en caso de que el formato introducido en el constructor sea incorrecto
-        if type(tipo) != Tipos_de_guerrero or type(arma) != Tipos_de_ataque:
-            raise TypeError("El formato introducido es incorrecto")
+        #if type(tipo) != Tipos_de_guerrero or type(arma) != Tipos_de_ataque:
+            #raise TypeError("El formato introducido es incorrecto")
 
-        if type(salud) != int or type(ataque) != float or type(defensa) != float:
+        if type(salud) != int or type(ataque) != int or type(defensa) != int:
             raise TypeError("El formato introducido es incorrecto")
 
         if not 1 <= salud <= 100:
@@ -46,3 +47,32 @@ class Warriors():
 
     def get_defenda(self):
         return self.__defensa
+
+    def set_tipo(self, tipo):
+        self.__tipo = tipo
+
+    def set_arma(self, arma):
+        self.__arma = arma
+
+    def set_salud(self, salud):
+        self.__salud = salud
+
+    def set_ataque(self, ataque):
+        self.__ataque = ataque
+
+    def set_defensa(self, defensa):
+        self.__defensa = defensa
+
+    def is_alive(self):
+        return self.is_vivo(self.__salud)
+
+    def fight_attack(self, warrior_to_attack):
+        e =None
+
+    def fight_defense(self, points_of_damage):
+        e = None
+
+
+tipo_1 = Tipos_de_ataque
+tipo_2 = Tipos_de_guerrero
+a = Warrior(tipo_2, tipo_1, 10, -2, -3)
