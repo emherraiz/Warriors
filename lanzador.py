447d80d10b542446d7d3dc2993ad4220ebd44f5c
Enrique Marina <eherraiz@myuax.com>
2022-07-08 17:40:17 +0200

    s

--- lanzador.py
+++ lanzador.py
@@ -1,6 +1,16 @@
+import random
+from enum import Enum
+from Vida import Vida
+from Tipos_de_armas import Tipos_de_armas
+from Tipos_de_guerreros import Tipos_de_guerreros
+from warrior import warrior
+from Guerrero_ofensivo import Guerrero_ofensivo
+from Guerrero_defensivo import Guerrero_defensivo
+from Super_guerrero import Super_guerrero
+from Coach import Coach
 
 
 
 
 def lanzar():
-    return 2
+    
