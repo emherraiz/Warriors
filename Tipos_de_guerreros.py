ffebc1b179c1e7922fbc55e45ed17eeed962b56f
Enrique Marina <eherraiz@myuax.com>
2022-07-08 18:08:55 +0200

    s

--- Tipos_de_guerreros.py
+++ Tipos_de_guerreros.py
@@ -4,3 +4,18 @@ class Tipos_de_guerreros(Enum):
     Gladiador = 2
     UFC = 3
     MMA = 4
+
+    @staticmethod
+    def from_str(str_guerrero):
+        str_guerrero = str_guerrero.lower()
+        if str_guerrero == 'boxer':
+            return Tipos_de_guerreros.BOXER
+        elif str_guerrero == 'gladiator':
+            return Tipos_de_guerreros.GLADIATOR
+        elif str_guerrero == 'ufc':
+            return Tipos_de_guerreros.UFC
+        elif str_guerrero == 'mma':
+            return Tipos_de_guerreros.MMA
+        else:
+            raise TypeError('No existe el tipo de guerrero' + str_guerrero)
+
