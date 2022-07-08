fecea4285dfcd804d7ca7b390416f5ef8843c0c5
Enrique Marina <eherraiz@myuax.com>
2022-07-08 16:58:27 +0200

    s

--- Vida.py
+++ Vida.py
@@ -1,3 +1,4 @@
+import random
 class Vida():
     # Le pasamos la vida que tiene al constructor
     def __init__(self, salud):
@@ -26,3 +27,5 @@ class Vida():
     # En caso de que usemos un print nos mostrará la cantidad de vida
     def __str__(self):
         return f'Vida: {self._salud}'
+
+print(random.randint(0,1))
